875ae8839d508268888b6d881b44d87af3e8c72c
Matt Perry <mattoperry@gmail.com>
2018-06-14 01:00:59 -0700

    fix regular expression variable

--- inc/class.trustproject.php
+++ inc/class.trustproject.php
@@ -11,9 +11,9 @@ class TrustProjectDomains {
 	
 	//checks if the domain is well-formed
 	private function validate( $domain ) {
-		return (preg_match("/^([a-z\d](-*[a-z\d])*)(\.([a-z\d](-*[a-z\d])*))*$/i", $domain_name) //valid chars check
-				&& preg_match("/^.{1,253}$/", $domain_name) //overall length check
-				&& preg_match("/^[^\.]{1,63}(\.[^\.]{1,63})*$/", $domain_name)   ); //length of each label
+		return ( preg_match( "/^([a-z\d](-*[a-z\d])*)(\.([a-z\d](-*[a-z\d])*))*$/i", $domain ) //valid chars check
+				&& preg_match( "/^.{1,253}$/", $domain ) //overall length check
+				&& preg_match( "/^[^\.]{1,63}(\.[^\.]{1,63})*$/", $domain ) ); //length of each label
 	
 	}	
 }
