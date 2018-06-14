e1b2162efe3a8848a15ddcfdf6535b3cd0afccfe
Matt Perry <mattoperry@gmail.com>
2018-06-14 01:02:37 -0700

    require that domains have two segments
    otherwise any string qualifies as a valid domain.

--- inc/class.trustproject.php
+++ inc/class.trustproject.php
@@ -11,9 +11,12 @@ class TrustProjectDomains {
 	
 	//checks if the domain is well-formed
 	private function validate( $domain ) {
+		
+		if ( count( explode( '.', $domain ) ) < 2 ) {
+			return false;
+		}
 		return ( preg_match( "/^([a-z\d](-*[a-z\d])*)(\.([a-z\d](-*[a-z\d])*))*$/i", $domain ) //valid chars check
 				&& preg_match( "/^.{1,253}$/", $domain ) //overall length check
 				&& preg_match( "/^[^\.]{1,63}(\.[^\.]{1,63})*$/", $domain ) ); //length of each label
-	
 	}	
 }
